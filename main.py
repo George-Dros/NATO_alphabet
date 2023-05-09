fe9f09f05b31dde3f834c078e4a1a7eb3665d401
George-Dros <georgedross25@gmail.com>
2023-05-09 15:52:16 +0300

    Added exceptions

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ import pandas
 
 data = pandas.read_csv('nato_phonetic_alphabet.csv')
 dictionary = {row.letter:row.code for (index, row) in data.iterrows()}
+print(dictionary)
 
 
-word = input("Word: ").upper()
-output_list = [dictionary[letter] for letter in word]
 
-print(output_list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [dictionary[letter] for letter in word]
+
+    except KeyError:
+        print("Only letters of the alphabet please.")
+        generate_phonetic()
+
+    else:
+        print(output_list)
+
+generate_phonetic()
 
